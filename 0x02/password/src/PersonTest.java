73ef30cd591ebf7ad5b7e4540d4885f5070c4a5c
julianacit <julianamt@ciandt.com>
2022-06-22 14:21:42 -0300

    Task 00 - JUnit 2

--- 0x02/password/src/PersonTest.java
+++ 0x02/password/src/PersonTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class PersonTest {
-    public static Person person;
+    private static Person person;
 
     @BeforeAll
     public static void setup() {
