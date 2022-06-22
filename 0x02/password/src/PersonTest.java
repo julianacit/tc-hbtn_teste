14497b40350fb9e92bed720d95befdae8a3e083d
julianacit <julianamt@ciandt.com>
2022-06-22 13:37:02 -0300

    Task 00 - JUnit 2

--- 0x02/password/src/PersonTest.java
+++ 0x02/password/src/PersonTest.java
@@ -11,42 +11,42 @@ public class PersonTest {
     }
 
     @ParameterizedTest
-    @ValueSource(Strings = {"PaulMcCartney2", "NeilArms2"})
+    @ValueSource(strings = {"PaulMcCartney2", "NeilArms2"})
     public void check_user_valid(String user) {
         person.setUser(user);
         Assertions.assertTrue(person.checkUser());
     }
 
     @ParameterizedTest
-    @ValueSource(Strings = {"Paul#McCartney", "Neil@Arms"})
+    @ValueSource(strings = {"Paul#McCartney", "Neil@Arms"})
     public void check_user_not_valid(String user) {
         person.setUser(user);
         Assertions.assertFalse(person.checkUser());
     }
 
     @ParameterizedTest
-    @ValueSource(Strings = {"123456789", "#$%1234"})
+    @ValueSource(strings = {"123456789", "#$%1234"})
     public void does_not_have_letters(String password) {
         person.setPassword(password);
         Assertions.assertFalse(person.checkPassword());
     }
 
     @ParameterizedTest
-    @ValueSource(Strings = {"Abcabcdefgh@", "#hbtn@%tc"})
+    @ValueSource(strings = {"Abcabcdefgh@", "#hbtn@%tc"})
     public void does_not_have_numbers(String password) {
         person.setPassword(password);
         Assertions.assertFalse(person.checkPassword());
     }
 
     @ParameterizedTest
-    @ValueSource(Strings = {"Abc@123", "12$@hbt"})
+    @ValueSource(strings = {"Abc@123", "12$@hbt"})
     public void does_not_have_eight_chars(String password) {
         person.setPassword(password);
         Assertions.assertFalse(person.checkPassword());
     }
 
     @ParameterizedTest
-    @ValueSource(Strings = {"abC123456$", "Hbtn@1234", "Betty@1#2", "Hbtn@123"})
+    @ValueSource(strings = {"abC123456$", "Hbtn@1234", "Betty@1#2", "Hbtn@123"})
     public void check_password_valid(String password) {
         person.setPassword(password);
         Assertions.assertTrue(person.checkPassword());
